32d96e538176710890fe320ee5d8cc0f2b9da909
MauAfo <mauafonsojr@hotmail.com>
2022-05-12 15:40:31 -0300

    novo início

---
+++ index2.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+
+	<title>Meu SEGUNDO site em PHP!!</title>
+
+		
+			<style type="text/css">
+				.linha {
+					font-weight: bold;
+					color: red;
+					padding-left: 10px;
+					line-height: 45px;
+					}
+			</style>
+			
+	
+	</head>
+
+<body>
+
+
+<?php
+	for ( $i = 0 ; $i < 10 ; $i++ ) 
+	{
+	
+	print ("<span class=\"linha\">Linha número " . $i . "</span><br />");
+	
+	}
+	
+		
+	
+?>
+
+
+</body>
+	
+	<script type="text/javascript">
+	$(document).ready(function() {
+	alert(PARABÉNS!!!!);
+	});
+	
+	</script>
+	
+</html>
